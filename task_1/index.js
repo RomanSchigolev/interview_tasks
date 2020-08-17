4e60da11bc64746a943448f6b3bfc535ecbb81b3
Roman Schigolev <r.shchigolev2018@yandex.ru>
2020-08-17 17:09:22 +0700

    task 1

---
+++ task_1/index.js
@@ -0,0 +1,22 @@
+// Написать функцию, в которую передается целое число.
+// Функция должна вывести горку как показано в примере с тем числом линий, которое было передано в функцию.
+
+const printHill = number => {
+    for(let i = 1; i <= number; i++) {
+        console.log("#".repeat(i))
+    }
+};
+console.log(printHill(5));
+
+// or
+let counter = "";
+const printHill = number => {
+  for (let i = 1; i <= number; i++) {
+    for (let j = 0; j < i; j++) {
+      counter = counter + "#";
+    }
+    counter = counter + "\n";
+  }
+  return counter.slice(0, -1);
+};
+console.log(printHill(5));
