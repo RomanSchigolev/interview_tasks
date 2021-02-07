5224e54e3dc1e08abf2026074223079b8753be9a
Roman Schigolev <r.shchigolev2018@yandex.ru>
2021-02-07 19:42:51 +0700

    minor changes

--- task_16/index.js
+++ task_16/index.js
@@ -4,7 +4,7 @@ console.log(countingNumbers1(1)); // 1
 console.log(countingNumbers1(10)); // 2
 console.log(countingNumbers1(100)); // 3
 
-const countingNumbers2 = (number) => String(number).split('').length;
+const countingNumbers2 = (number) => number.toString().length;
 
 console.log(countingNumbers2(1)); // 1
 console.log(countingNumbers2(10)); // 2
